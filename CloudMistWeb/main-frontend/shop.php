b9704b65fca592a6b64fe6c1bd45f10ab066ca0a
BY <byipster@hotmail.com>
2015-04-12 18:29:16 -0600

    Search is done on shop.php

--- CloudMistWeb/main-frontend/shop.php
+++ CloudMistWeb/main-frontend/shop.php
@@ -5,6 +5,56 @@ CPSC471
     require_once '../backend/gamer_verify.php';
     $user = ($_SESSION['g_user']);
 
+    function searchResults() {
+        if(isset($_POST['searchText']))  {
+            require '../backend/connect.php';
+            $searchText = $_POST['searchText'];
+            if(!empty($searchText)) {
+                $search_query = "SELECT game_id, name, price, genre, release_date FROM game "
+                        . "WHERE game_id LIKE '%$searchText%'"
+                        . "OR name LIKE '%$searchText%'"
+                        . "OR genre LIKE '%$searchText%'";
+                $result = mysqli_query($conn, $search_query);
+
+                if($result -> num_rows> 0) {
+
+                    echo '<table id="gameList"><tbody><tr>'
+                            . '<th>ID</th>'
+                            . '<th>NAME</th>'
+                            . '<th>PRICE</th>'
+                            . '<th>GENRE</th>'
+                            . '<th>RELEASE DATE</th>'
+                            . '<th>MORE INFO</th>'
+                        . '</tr>';
+
+                    //output data
+                    while($row = $result -> fetch_assoc()) {
+                        $game_id = $row["game_id"];
+                        echo "<tr><td>".$row["game_id"]."</td>"
+                               . "<td>".$row["name"]."</td>"
+                               . "<td>".$row["price"]."</td>"
+                               . "<td>".$row["genre"]."</td>"
+                               . "<td>".$row["release_date"]."</td>"
+                               . '<td><a href="../main-frontend/currentGame.php?game_id='.$game_id.'"> Buy Game </a></td>'
+                            . "</tr>";
+                    }
+                    echo "</tbody></table>";
+
+                }
+                else {
+                    echo "0 results";
+                }
+
+            }
+            else {
+                filldiv();
+            }
+        }
+        else {
+            filldiv();
+        }
+    }
+    
     function filldiv() { 
         require '../backend/connect.php';
         $sql_request = "SELECT game_id, name, price, genre, release_date FROM game";
@@ -79,9 +129,15 @@ CPSC471
         </div>
         
         <div id="content">
-            <h1>SHOP</h1>
+            <div class="title"><h1>SHOP</h1></div>
+            <div class="searchBar">
+                <form method="post" action="shop.php"> 
+                        <input type="text" id="searchBox" name="searchText"> 
+                        <input type="submit" value="Search" class="form-field">
+                </form>
+            </div>
             <h2>Games List</h2>
-            <?php filldiv() ?>
+            <?php   searchResults();?>
         </div>
     </div>
 </body>
